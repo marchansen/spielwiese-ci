bb033f28b114751190b365c3697f33510b7a0c57
Marc Hansen <marc.hansen@bbv.ch>
2013-07-10 13:36:47 +0200

    bla

---
+++ WPFTest/CalculatorService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTest
+{
+    public class CalculatorService : ICalculator
+    {
+        public double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public double Subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public double Divide(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
