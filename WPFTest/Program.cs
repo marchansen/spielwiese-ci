72e21a155da68f4378d17cf0d27fb6722346763a
Marc Hansen <marc.hansen@bbv.ch>
2013-07-11 12:29:00 +0200

    grgerg

--- WPFTest/Program.cs
+++ WPFTest/Program.cs
@@ -1,14 +1,10 @@
-﻿using Microsoft.Office.Interop.Excel;
-
+﻿
 namespace WPFTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Application excel = new Application();
-            Workbook workbook = excel.Workbooks.Add();
-            excel.Visible = true;
 
         }
     }
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void XUnitSuccessTest()
+        {
+            Assert.Equal(4,4);
+        }
+
+        [Fact]
+        public void XUnitFailureTest()
+        {
+            Assert.Equal(4, 5);
+        }
+    }
+}
